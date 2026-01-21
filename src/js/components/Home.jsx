7cd251abe4d3d65e6f75b570cd43b41b2bfa3134
1989212 <jessica_villamizar.c@hotmail.com>
2026-01-21 22:35:56 +0000

    inicio de proyecto

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const Home = () => {
+
+	const [task, setTask]= useState("")
+	const [taskList, setTaskList]= useState(["Hacer la Tarea", "Limpiar la casa", "Hacer Ejercicios","Dormir la Sieta",])
+	function addTask(){
+		setTaskList([...taskList,task])
+	}
+	
+	
+	
+	
+	return (
+		<div className="text-center d-flex flex-column">
+        
+
+
+			<h1 className="text-center mt-5">Todo List!</h1>
+			<input type="text" 
+				className="form-control w-25 mx-auto" 
+				placeholder="What needs to be done" 
+				onChange={(event) => setTask (event.target.value)}
+				onKeyUp={(event) => {
+					if (event.key === "Enter") {
+						setTaskList([...taskList, task]);
+						setTask("");
+					}
+					}
+				}
+			/>
+			{/* <button className="btn btn-primary" style={{ width: "180px", height: "45px", fontSize: "16px" }} 
+			onClick={() =>addTask ()}>Agrerar Tarea
+			</button> */}
+			
+			<p> {task} </p>
+			{taskList.map((taskItem, index) => <p key={index}>{taskItem}
+				<button onClick={()=>setTaskList(taskList.filter((item,ind)=> ind !== index))}
+				className="btn-close"></button>
+				</p>)}
+		
 		</div>
 	);
 };
