3b629e780297db1158b5a09d2ddcc3c8900aeac9
1989212 <jessica_villamizar.c@hotmail.com>
2026-01-22 04:20:09 +0000

    nuevos cambios

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,51 +1,64 @@
 import React, {useState} from "react";
+import "../../styles/index.css";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-
 const Home = () => {
 
 	const [task, setTask]= useState("")
-	const [taskList, setTaskList]= useState([])
+	const [taskList, setTaskList]= useState(["Lavar la Ropa", "Hacer Ejercicios", "Ir al Supermercado", "Pasear al Perro", "Dormir una Siesta",])
 	function addTask(){
 		setTaskList([...taskList,task])
 	}
 	
 	
 	
-	
 	return (
 		<div className="text-center d-flex flex-column">
-        
+
+			<div className="Todo-box mx-auto mt-5">  
 
 
-			<h1 className="text-center mt-5">Todos</h1>
+			<h1 className="text-center mb-3">Todos</h1>
 			<input type="text" 
-				className="form-control w-25 mx-auto" 
-				placeholder="What needs to be done" 
-				onChange={(event) => setTask (event.target.value)}
-				onKeyUp={(event) => {
-					if (event.key === "Enter") {
+				className="form-control" 
+				placeholder="What needs to be done?" 
+				value={task}
+				onChange={event=>setTask(event.target.value)}
+				onKeyUp={(e) => {
+					if (e.key === "Enter") {
 						setTaskList([...taskList, task]);
 						setTask("");
 					}
-					}
-				}
+					}}
 			/>
-			{/* <button className="btn btn-primary" style={{ width: "180px", height: "45px", fontSize: "16px" }} 
-			onClick={() =>addTask ()}>Agrerar Tarea
-			</button> */}
+			<ul className="list-unstyled mb-0">
+			{taskList.length === 0 ? (
+			<li className= "empty-msg">No hay tareas, añadir tareas.</li>
+			) : (
+				taskList.map((task, index) => (
+					<li className="todo-item" key={index}>
+						<span>{task}</span>
 
-			{taskList.length === 0 && <p className="mt-3">No hay tareas, añadir tareas</p>}
-			
-			<p> {task} </p>
-			{taskList.map((taskItem, index) => <p key={index}>{taskItem}
-				<button onClick={()=>setTaskList(taskList.filter((item,ind)=> ind !== index))}
-				className="btn-close"></button>
-				</p>)}
-		
+						<span
+						className="delete-btn"
+						onClick={() =>
+							setTaskList(taskList.filter((item, ind) => ind !== index))
+						}
+						>
+							x
+						</span>
+					</li>
+			)) 
+		)}
+
+			</ul>
+			<p className="count">
+				{taskList.length} item{taskList.length !== 1 ? "s" : ""}left 
+			</p>
+			</div>
 		</div>
 	);
 };
